445e202052e360fc0386d540918588bc50f4fb68
Tadeáš Kótka <tadekotk@gmail.com>
2026-05-08 18:20:17 +0200

    tomas > obchod

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,34 +1,69 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Footer() {
-    return (
-        <footer className="bg-[url('/footer.jpg')] text-white p-4 w-full h-[400px] flex flex-col justify-center items-center">
-            <div className="container mx-auto h-full">
-                <div className="flex flex-row justify-center items-center w-full h-[60%]">
-                    <div className="flex flex-col justify-center items-center w-1/3 h-full">
-                        <ul className="">
-                            <li><Link href="/">ÚVOD</Link></li>
-                            <li><Link href="/nabidka">NABÍDKA</Link></li>
-                            <li><Link href="/vinarstvi">VINAŘSTVÍ</Link></li>
-                            <li><Link href="/blog">BLOG</Link></li>
-                            <li><Link href="/kontakt">KONTAKT</Link></li>
-                        </ul>
-                    </div>
-                    <div className="flex flex-col justify-center items-center w-1/3 h-full">
-                        <h2 className="text-xl w-full text-left">KONTAKT</h2>
-                        <p className="text-left text-xs w-full">Email: tomas@hungarianwine.cz</p>
-                        <p className="text-left text-xs w-full">Tel: +420 736 502 958</p>
-                        <p className="text-left text-xs w-full">Facebook: <a href="https://www.facebook.com/hungarianwine.cz/">hungarianwine.cz</a></p>
-                    </div>
-                    <div className="flex flex-col justify-center items-center w-1/3 h-full">
-                        <ul className="text-right text-xs">
-                            <li><a href="/obchodni_podminky.pdf" target="_blank" rel="noopener noreferrer">Obchodní podmínky</a></li>
-                            <li><a href="/zasady_ochrany.pdf" target="_blank" rel="noopener noreferrer">Zpracování osobních údajů</a></li>
-                            <li><Link href="/cookies">Cookies</Link></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="bg-[url('/footer.jpg')] text-white p-4 w-full h-[400px] flex flex-col justify-center items-center">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-row justify-center items-center w-full h-[60%]">
+          <div className="flex flex-col justify-center items-center w-1/3 h-full">
+            <ul className="">
+              <li>
+                <Link href="/">ÚVOD</Link>
+              </li>
+              <li>
+                <Link href="/nabidka">NABÍDKA</Link>
+              </li>
+              <li>
+                <Link href="/vinarstvi">VINAŘSTVÍ</Link>
+              </li>
+              <li>
+                <Link href="/blog">BLOG</Link>
+              </li>
+              <li>
+                <Link href="/kontakt">KONTAKT</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="flex flex-col justify-center items-center w-1/3 h-full">
+            <h2 className="text-xl w-full text-left">KONTAKT</h2>
+            <p className="text-left text-xs w-full">
+              Email: obchod@hungarianwine.cz
+            </p>
+            <p className="text-left text-xs w-full">Tel: +420 736 502 958</p>
+            {/*<p className="text-left text-xs w-full">
+              Facebook:{" "}
+              <a href="https://www.facebook.com/hungarianwine.cz/">
+                hungarianwine.cz
+              </a>
+            </p>*/}
+          </div>
+          <div className="flex flex-col justify-center items-center w-1/3 h-full">
+            <ul className="text-right text-xs">
+              <li>
+                <a
+                  href="/obchodni_podminky.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Obchodní podmínky
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/zasady_ochrany.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Zpracování osobních údajů
+                </a>
+              </li>
+              <li>
+                <Link href="/cookies">Cookies</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,50 +1,75 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import Nav from "../nav";
 import Footer from "../footer";
 import Top from "../top";
 import { useEffect } from "react";
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Kontakt() {
+  const [isMobile, setIsMobile] = useState(false);
 
-    const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 1200);
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-    useEffect(() => {
-        const handleResize = () => setIsMobile(window.innerWidth < 1200);
-        handleResize();
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    return (
-        <div>
-            <Top />
-            <Nav />
-            <main className='flex justify-center items-center w-full flex-col'>
-                <div className={isMobile ? "flex flex-wrap w-full justify-center items-center bg-white" : "flex flex-wrap w-[60%] justify-center bg-white"}>
-                    <div className={isMobile ? "flex flex-col justify-center items-center w-full bg-white p-3" : "flex flex-wrap justify-center items-center w-[40%] bg-white"}>
-                        <div className="w-full flex-col justify-center items-center">
-                            <h1 className={isMobile ? "text-2xl text-[#007A37]" : "text-3xl text-[#007A37]"}>HUNGARIAN WINE</h1>
-                            <p className="p-2">E-MAIL <br /> tomas@hungarianwine.cz</p>
-                            <p className="p-2">TELEFON <br /> +420 736 502 958</p>
-                            <p className="p-2">PONDĚLÍ - PÁTEK <br /> Od 9:00 do 18:00</p>
-                        </div>
-                    </div>
-                    {isMobile ? null :
-                        <div className="w-[40%] h-[80%] flex flex-wrap justify-center items-center">
-                            <Image
-                                src="/mail-ico.png"
-                                width={300}
-                                height={300}
-                                alt="ikona mailu"
-                            />
-                        </div>
-                    }
-                    <Footer />
-                </div>
-            </main>
+  return (
+    <div>
+      <Top />
+      <Nav />
+      <main className="flex justify-center items-center w-full flex-col">
+        <div
+          className={
+            isMobile
+              ? "flex flex-wrap w-full justify-center items-center bg-white"
+              : "flex flex-wrap w-[60%] justify-center bg-white"
+          }
+        >
+          <div
+            className={
+              isMobile
+                ? "flex flex-col justify-center items-center w-full bg-white p-3"
+                : "flex flex-wrap justify-center items-center w-[40%] bg-white"
+            }
+          >
+            <div className="w-full flex-col justify-center items-center">
+              <h1
+                className={
+                  isMobile
+                    ? "text-2xl text-[#007A37]"
+                    : "text-3xl text-[#007A37]"
+                }
+              >
+                HUNGARIAN WINE
+              </h1>
+              <p className="p-2">
+                E-MAIL <br /> obchod@hungarianwine.cz
+              </p>
+              <p className="p-2">
+                TELEFON <br /> +420 736 502 958
+              </p>
+              <p className="p-2">
+                PONDĚLÍ - PÁTEK <br /> Od 9:00 do 18:00
+              </p>
+            </div>
+          </div>
+          {isMobile ? null : (
+            <div className="w-[40%] h-[80%] flex flex-wrap justify-center items-center">
+              <Image
+                src="/mail-ico.png"
+                width={300}
+                height={300}
+                alt="ikona mailu"
+              />
+            </div>
+          )}
+          <Footer />
         </div>
-    );
+      </main>
+    </div>
+  );
 }
